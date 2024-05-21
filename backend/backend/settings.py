7ce01b808244b344b15ba6e47b4a200d5afec554
Kehinde Temitayo <kehindetemitayo.b@gmail.com>
2024-05-21 15:40:36 +0100

    seventh commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -101,8 +101,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER':'postgres',
+        'PASSWORD':'hGiQxzTggeTBMhNOuAStgrWyMIIClVqb',
+        'HOST':'monorail.proxy.rlwy.net',
+        'PORT':'29580',
     }
 }
 
