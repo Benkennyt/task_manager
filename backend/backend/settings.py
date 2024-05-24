9bb7928477f37d5933954634826fbb0494b4bd02
Kehinde Temitayo <kehindetemitayo.b@gmail.com>
2024-05-24 14:48:36 +0100

    db6

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,12 +28,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6rcn!ylqc!7s@wit^z6+(dl8^kxec5ptk!&hhn4$ghcj!#45tg'
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == 'true'
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOST").split(" ")
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
