1623a319203253dfccde9bb9cc5560f20f300cfa
Kehinde Temitayo <kehindetemitayo.b@gmail.com>
2024-05-29 19:20:18 +0100

    some css

--- frontend/src/features/authentication/login/LoginForm.tsx
+++ frontend/src/features/authentication/login/LoginForm.tsx
@@ -14,9 +14,8 @@ import { ACCESS_TOKEN } from "../../../constants";
 
 const LoginForm = () => {
   const [inputField, setInputField] = useState(new LoginForm1());
-  const [showPassword, setShowPassword] = useState(false);
-
-  const {isLoginLoading, loginErrorData} = useSelector((state:any) => state.user)
+  const [passwordType, setPasswordType] = useState('');
+  const { isLoginLoading, loginErrorData } = useSelector((state: any) => state.user)
   const navigate = useNavigate()
   const token = localStorage.getItem(ACCESS_TOKEN)
 
@@ -25,94 +24,74 @@ const LoginForm = () => {
       navigate('/home')
     }
   }, [isLoginLoading])
-  
-  const handleLogin = async (values:any) => {
+
+  const handleLogin = async (values: any) => {
     dispatch(login(values))
   }
-   
+
   const dispatch = useAppDispatch()
 
-    const handlePasswordShowHide = () => {
-    if (showPassword) {
-      setShowPassword(false)
-    } else{
-      setShowPassword(true)
-    }
-  }
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputField({...inputField, [e.target.name]: e.target.value}) 
+    setInputField({ ...inputField, [e.target.name]: e.target.value })
   }
 
 
   return (
     <div className="sign-in-fm">
       <div className="form-1">
-            <h1 className="logo">TaskBender</h1>
-            <h3>Sign In</h3>
-            <Formik
-                initialValues={inputField}
-                enableReinitialize
-                onSubmit={(values) => 
-                  handleLogin(values)
-                  }
-                validationSchema={signInSchema}
-            >
-                {({ handleSubmit,
-                    handleBlur,
-                    values,
-                    errors,
-                    touched,
-                    }) => (
-                    <Form onSubmit={handleSubmit} autoComplete='off'>
-                        <InputTemp 
-                            id="username"
-                            name="username" 
-                            inputType="text" 
-                            onBlur={handleBlur}
-                            onChange={handleChange}
-                            value={values.username}
-                            placeholder="Username"
-                            errors={errors}
-                            touched={touched}
-                            autoComplete="off"
-                        />
-                        <InputTemp 
-                            id="password" 
-                            name="password" 
-                            inputType={showPassword ? 'text': "password" } 
-                            onBlur={handleBlur}
-                            onChange={handleChange}
-                            value={values.password}
-                            placeholder="Password"
-                            errors={errors}
-                            touched={touched}
-                            handlePasswordShowHide={handlePasswordShowHide}
-                            showPassword={showPassword}
-                        />
-                        { loginErrorData && <p className="login-err">{loginErrorData}</p>}
-                        {isLoginLoading ? <button disabled className="sign-in-btn1" type="submit">
-                          <i className="fa fa-spinner fa-spin"></i>Loading...
-                        </button> : <button className="sign-in-btn1" type="submit">
-                          Sign In
-                        </button>}
-                    </Form>
-                )}
-            </Formik>
-            <p className="dont-hv-acc">Don't have an account? <span onClick={() => navigate('/sign-up')}>Sign Up</span></p>
-            {/* <div className="other-signin-option">
-              <button
-                
-                className="btn btn-transition google-btn"
-              >
-                <GoogleIcon />
-                Google
-              </button>
-              <button className="btn btn-transition facebook-btn">
-                <FacebookIcon />
-                Facebook
-              </button>
-            </div> */}
+        <h1 className="logo">TaskBender</h1>
+        <h3>Sign In</h3>
+        <Formik
+          initialValues={inputField}
+          enableReinitialize
+          onSubmit={(values) =>
+            handleLogin(values)
+          }
+          validationSchema={signInSchema}
+        >
+          {({ handleSubmit,
+            handleBlur,
+            values,
+            errors,
+            touched,
+          }) => (
+            <Form onSubmit={handleSubmit} autoComplete='off'>
+              <InputTemp
+                id="username"
+                name="username"
+                inputType="text"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.username}
+                placeholder="Username"
+                errors={errors}
+                touched={touched}
+                autoComplete="off"
+              />
+              <InputTemp
+                id="password"
+                name="password"
+                inputType={passwordType === 'password' ? 'text' : "password"}
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.password}
+                placeholder="Password"
+                errors={errors}
+                touched={touched}
+                setPasswordType={setPasswordType}
+                passwordType={passwordType}
+              />
+              {loginErrorData && <p className="login-err">{loginErrorData}</p>}
+              {isLoginLoading ? <button disabled className="sign-in-btn1" type="submit">
+                <i className="fa fa-spinner fa-spin"></i>Loading...
+              </button> : <button className="sign-in-btn1" type="submit">
+                Sign In
+              </button>}
+            </Form>
+          )}
+        </Formik>
+        <p className="dont-hv-acc">Don't have an account? <span onClick={() => navigate('/sign-up')}>Sign Up</span></p>
       </div>
       <div className="sign-in-right">
         <h1>Welcome Back :)</h1>
